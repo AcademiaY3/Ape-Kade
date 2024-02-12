3bd312f89a757229079e61d34b96df24feadf3f6
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2024-02-12 14:16:45 +0530

    aboutPage

--- frontend/user/src/App.jsx
+++ frontend/user/src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
 import React from 'react'
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
-import Home from './Pages/AppStack/Home/Home'
+import AppStack from './Pages/AppStack/AppStack'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Home/>
-      
-      <Footer/>
+      <AppStack/>
     </>
   )
 }
