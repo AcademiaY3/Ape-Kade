403c558530565f91cfffda7e760f1b997646ab42
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2024-02-13 14:56:32 +0530

    forgotPassword

--- frontend/user/src/App.jsx
+++ frontend/user/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import AppStack from './Pages/AppStack/AppStack'
 import SignIn from './Pages/AuthStack/SignIn/SignIn'
 import SignUp from './Pages/AuthStack/SignUp/SignUp'
+import ForgotPassword from './Pages/AuthStack/ForgotPassword/ForgotPassword'
 
 function App() {
 
@@ -10,7 +11,8 @@ function App() {
     <>
       {/* <AppStack/> */}
       {/* <SignIn/> */}
-      <SignUp/>
+      {/* <SignUp/> */}
+      <ForgotPassword/>
     </>
   )
 }
