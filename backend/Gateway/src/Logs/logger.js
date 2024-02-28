d4b98d0c8df01f226ecf16f65bd5f78fe51bb5a5
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2024-02-28 22:35:29 +0530

    loggerOnDB

--- backend/Gateway/src/Logs/logger.js
+++ backend/Gateway/src/Logs/logger.js
@@ -1,5 +1,6 @@
 import { createLogger, transports, format } from 'winston'
-import {v4 as uuid} from 'uuid'
+import { v4 as uuid } from 'uuid'
+import {MongoDB} from 'winston-mongodb'
 
 const logger = (req, res, next) => {
     const clientIp = req.ip || req.connection.remoteAddress
@@ -7,24 +8,38 @@ const logger = (req, res, next) => {
 
     const gatewayLogger = createLogger({
         level: 'info',
-        format: format.combine(format.timestamp(), format.json()),
-        defaultMeta: { service: 'gateway-service' },
+        // add format.metadata() to include the aditional needed fields
+        format: format.combine(format.timestamp(),format.metadata() ,format.json()),
+        defaultMeta: {
+            service: 'gateway-service',
+            id,
+            method: req.method,
+            clientIp,
+            origin: req.headers.origin,
+            url: req.url,
+            protocol: req.protocol,
+            host: req.get('host'),
+            path: req.path,
+            query: req.query,
+            cookies: req.cookies,
+            headers: req.headers,
+            // statusCode: res.statusCode,
+            userAgent: req.get('User-Agent')
+        },
         transports: [
             new transports.File({
                 filename: 'gateway.log'
+            }),
+            new transports.MongoDB({
+                db: process.env.MONGO_URI,
+                options: {
+                    useUnifiedTopology: true
+                },
+                collection: 'logs'
             })
         ],
     })
-
-    gatewayLogger.info({
-        id,
-        method: req.method,
-        clientIp,
-        origin: req.headers.origin,
-        url: req.url,
-        statusCode: res.statusCode,
-        userAgent: req.get('User-Agent')
-    })
+    gatewayLogger.info()
 
     next()
 
--- backend/Gateway/src/app.js
+++ backend/Gateway/src/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import corsOption from "./Config/CorsConfig.js";
 import logger from "./Logs/logger.js";
+import proxy from 'express-http-proxy'
 
 dotenv.config()
 const app = express()
@@ -13,9 +14,15 @@ app.use(express.json())
 app.use(logger)
 
 app.get('/', (req, res) => {
-    return res.status(200).json({'msg':'gateway online'})
+    return res.status(200).json({ 'msg': 'gateway online' })
 })
 
+// proxies
+app.use('/product',proxy("https://www.youtube.com"))
+
+app.use((req, res) => {
+    return res.status(404).json({'msg':'invalid routes'})
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
